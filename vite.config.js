c02c67730de96e1b6231f05958ffda637dfa1d75
Enzo de Araújo <enzodearaujogrellmann@hotmail.com>
2024-12-13 09:06:43 -0300

    Logo update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/', // Garante o caminho base correto
-});
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
