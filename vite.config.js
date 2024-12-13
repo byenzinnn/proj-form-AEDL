fe4e8dd302e3fe793a21321448507a3420098ab3
Enzo de Araújo <enzodearaujogrellmann@hotmail.com>
2024-12-13 08:55:43 -0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,11 @@ export default defineConfig({
   base: process.env.VITE_BASE_URL || '/', // Garante o caminho base correto
 });
 
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/proj-form-AEDL/", // Nome do repositório no GitHub
+});
